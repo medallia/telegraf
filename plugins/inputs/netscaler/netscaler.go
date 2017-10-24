25662ce91810941a291dbe09ec4cedb2ff3c72fa
mspinassi <mspinassi@medallia.com>
2017-10-24 14:00:48 -0300

    Switching spaces for tabs

--- plugins/inputs/netscaler/netscaler.go
+++ plugins/inputs/netscaler/netscaler.go
@@ -1,16 +1,16 @@
 package netscaler
 
 import (
-  "net/http"
-  "io/ioutil"
-  "time"
-  "crypto/tls"
-  "log"
-  "encoding/json"
-  "reflect"
+	"crypto/tls"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"reflect"
+	"time"
 
-  "github.com/influxdata/telegraf"
-  "github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
 /*
@@ -29,13 +29,13 @@ var sampleConfig = `
   # ApiPath = "/nitro/v1/stat"
 `
 
-var tr = &http.Transport {
-    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+var tr = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 }
 
 var httpClient = &http.Client{
-  Timeout: time.Second * 3,
-  Transport: tr,
+	Timeout:   time.Second * 3,
+	Transport: tr,
 }
 
 /*
@@ -43,18 +43,18 @@ var httpClient = &http.Client{
  * request to Netscaler's API.
  */
 type NSResponse interface {
-  PublishMetrics(acc telegraf.Accumulator)
+	PublishMetrics(acc telegraf.Accumulator)
 }
 
 /*
  * Main struct used by Telegraf for this plugin.
  */
 type Netscaler struct {
-  Host string
-  Username string
-  Password string
-  Https bool
-  ApiPath string
+	Host     string
+	Username string
+	Password string
+	Https    bool
+	ApiPath  string
 }
 
 func (n *Netscaler) Description() string {
@@ -62,7 +62,7 @@ func (n *Netscaler) Description() string {
 }
 
 func (n *Netscaler) SampleConfig() string {
-  return sampleConfig
+	return sampleConfig
 }
 
 /*
@@ -71,65 +71,65 @@ func (n *Netscaler) SampleConfig() string {
  * does the connection.
  */
 func (n *Netscaler) fetchResource(url_path string) ([]byte, error) {
-  var http_prefix string
-  var body []byte
-  var err error = nil
+	var http_prefix string
+	var body []byte
+	var err error = nil
 
-  if (n.Https) {
-    http_prefix = "https://"
-  } else {
-    http_prefix = "http://"
-  }
-  url := http_prefix + n.Host + n.ApiPath + url_path
+	if n.Https {
+		http_prefix = "https://"
+	} else {
+		http_prefix = "http://"
+	}
+	url := http_prefix + n.Host + n.ApiPath + url_path
 
-  req, err := http.NewRequest("GET", url, nil)
-  if (err != nil) {
-    return body, err
-  }
-  // Headers required by Netscaler's API interface
-  req.Header.Add("X-NITRO-USER", n.Username)
-  req.Header.Add("X-NITRO-PASS", n.Password)
-  req.Header.Add("Content-type", "application/x-www-form-urlencoded")
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return body, err
+	}
+	// Headers required by Netscaler's API interface
+	req.Header.Add("X-NITRO-USER", n.Username)
+	req.Header.Add("X-NITRO-PASS", n.Password)
+	req.Header.Add("Content-type", "application/x-www-form-urlencoded")
 
-  resp, err := httpClient.Do(req)
-  if (err != nil) {
-    log.Fatal("Could not connect to the host:", err)
-    return body, err
-  }
-  defer resp.Body.Close()
-  if (resp.StatusCode != 200) {
-      return body, err
-  }
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatal("Could not connect to the host:", err)
+		return body, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return body, err
+	}
 
-  body, err = ioutil.ReadAll(resp.Body)
-  return body, err
+	body, err = ioutil.ReadAll(resp.Body)
+	return body, err
 }
 
 // Dispatches metrics to Telegraf
 func (n *Netscaler) Gather(acc telegraf.Accumulator) error {
-  resources := make(map[string]interface{
-    PublishMetrics(acc telegraf.Accumulator)
-  })
-  resources["/lbvserver/"] = new(LBVResponse)
+	resources := make(map[string]interface {
+		PublishMetrics(acc telegraf.Accumulator)
+	})
+	resources["/lbvserver/"] = new(LBVResponse)
 
-  for resource, resStruct := range resources {
-    // Connect to Netscaler and gather the result from the resource
-    body_raw, err  := n.fetchResource(resource)
-    if (err != nil) {
-      log.Fatal("Coud not get results from resource %s: %s",
-        resource, err.Error())
-      return err
-    }
-    if err := json.Unmarshal(body_raw, &resStruct); err != nil {
-      log.Fatalf("Could not parse json from resource %s: %s",
-        resource, err.Error())
-      return err
-    }
-    // Call the implementation of PublishMetrics in the interface.
-    // Here is where metrics are actually dispatched.
-    resStruct.PublishMetrics(acc)
-  }
-  return nil
+	for resource, resStruct := range resources {
+		// Connect to Netscaler and gather the result from the resource
+		body_raw, err := n.fetchResource(resource)
+		if err != nil {
+			log.Fatal("Coud not get results from resource %s: %s",
+				resource, err.Error())
+			return err
+		}
+		if err := json.Unmarshal(body_raw, &resStruct); err != nil {
+			log.Fatalf("Could not parse json from resource %s: %s",
+				resource, err.Error())
+			return err
+		}
+		// Call the implementation of PublishMetrics in the interface.
+		// Here is where metrics are actually dispatched.
+		resStruct.PublishMetrics(acc)
+	}
+	return nil
 }
 
 /*
@@ -141,19 +141,19 @@ func (n *Netscaler) Gather(acc telegraf.Accumulator) error {
  *           of the tag.
  */
 func getTags(v reflect.Value) map[string]string {
-  tags := make(map[string]string)
-  const tag string = "tag"
+	tags := make(map[string]string)
+	const tag string = "tag"
 
-  for i := 0; i < v.NumField(); i++ {
-    if ctag, ok := v.Type().Field(i).Tag.Lookup("typeofmetric"); ok {
-      if (ctag == tag) {
-        fieldName := v.Type().Field(i).Name
-        fieldValue := reflect.Indirect(v).Field(i).Interface().(string)
-        tags[fieldName] = fieldValue
-      }
-    }
-  }
-  return tags
+	for i := 0; i < v.NumField(); i++ {
+		if ctag, ok := v.Type().Field(i).Tag.Lookup("typeofmetric"); ok {
+			if ctag == tag {
+				fieldName := v.Type().Field(i).Name
+				fieldValue := reflect.Indirect(v).Field(i).Interface().(string)
+				tags[fieldName] = fieldValue
+			}
+		}
+	}
+	return tags
 }
 
 /*
@@ -165,30 +165,30 @@ func getTags(v reflect.Value) map[string]string {
                           in the struct as `typeofmetric:<string>`.
  * @Returns: A map of the type [string]string with the name and value
  *           of the tag.
- */
+*/
 func getMetrics(v reflect.Value, metricTagType string) map[string]interface{} {
-  m := make(map[string]interface{})
-  for i := 0; i < v.NumField(); i++ {
-    if ctag, ok := v.Type().Field(i).Tag.Lookup("typeofmetric"); ok {
-      if (ctag == metricTagType) {
-        fieldName := v.Type().Field(i).Name
-        fieldValue := reflect.Indirect(v).Field(i).Interface()
-        m[fieldName] = fieldValue
-      }
-    }
-  }
-  return m
+	m := make(map[string]interface{})
+	for i := 0; i < v.NumField(); i++ {
+		if ctag, ok := v.Type().Field(i).Tag.Lookup("typeofmetric"); ok {
+			if ctag == metricTagType {
+				fieldName := v.Type().Field(i).Name
+				fieldValue := reflect.Indirect(v).Field(i).Interface()
+				m[fieldName] = fieldValue
+			}
+		}
+	}
+	return m
 }
 
 // Initializes the plugin
 func init() {
-  inputs.Add("netscaler", func() telegraf.Input {
-    return &Netscaler{
-      Host: "127.0.0.1",
-      Username: "admin",
-      Password: "admin",
-      Https: false,
-      ApiPath: "/nitro/v1/stat",
-    }
-  })
+	inputs.Add("netscaler", func() telegraf.Input {
+		return &Netscaler{
+			Host:     "127.0.0.1",
+			Username: "admin",
+			Password: "admin",
+			Https:    false,
+			ApiPath:  "/nitro/v1/stat",
+		}
+	})
 }
